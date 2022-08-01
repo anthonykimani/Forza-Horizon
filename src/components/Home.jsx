23094d11c17c074642bc196f2a47d4e3fab02714
anthonykimani <anthonykimani@student.moringaschool.com>
2022-08-01 07:31:02 +0300

    Setups boilerplate

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return ( 
+        <div className="h-screen w-full">
+            <video autoPlay="autoplay" loop="loop" muted="muted" playsInline className="absolute top-0 left-0 z-0 h-full object-cover object-center" >
+                <source src="https://forza.azureedge.net/strapi-uploads/assets/F_Mnet_Splash_Video_5_Mbps_1_Frame_Short_050654d1eb.mp4" />
+            </video>
+        </div>
+     );
+}
+ 
+export default Home;
